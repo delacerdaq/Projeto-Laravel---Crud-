260a234bed8f49e52b28dbe089c373c35657de6d
delacerdaq <analuizadelacerda@gmail.com>
2024-10-06 00:30:51 -0300

    urls referring to the disciplines

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,10 @@ Route::post('/teachers/editar/{id}', [App\Http\Controllers\TeacherController::cl
 Route::get('/teachers/excluir/{id}', [App\Http\Controllers\TeacherController::class, 'delete' ]);
 Route::delete('/teachers/excluir/{id}', [App\Http\Controllers\TeacherController::class, 'destroy' ])->name('excluir_professor');
 
-
+Route::get('/disciplines/novo', [App\Http\Controllers\DisciplineController::class, 'create' ]);
+Route::post('/disciplines/novo', [App\Http\Controllers\DisciplineController::class, 'store' ])->name('registrar_disciplina');
+Route::get('/disciplines/ver/{id}', [App\Http\Controllers\DisciplineController::class, 'show' ]);
+Route::get('/disciplines/editar/{id}', [App\Http\Controllers\DisciplineController::class, 'edit' ]);
+Route::post('/disciplines/editar/{id}', [App\Http\Controllers\DisciplineController::class, 'update' ])->name('alterar_disciplina');
+Route::get('/disciplines/excluir/{id}', [App\Http\Controllers\DisciplineController::class, 'delete' ]);
+Route::delete('/disciplines/excluir/{id}', [App\Http\Controllers\DisciplineController::class, 'destroy' ])->name('excluir_disciplina');
